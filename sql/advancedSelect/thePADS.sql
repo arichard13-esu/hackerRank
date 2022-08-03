8c885f7d926fded73461f8e58d9a49c6e896fe10
Aaron Richardson <55447931+arichard13-esu@users.noreply.github.com>
2022-08-03 14:14:17 -0400

    Add files via upload

---
+++ sql/advancedSelect/thePADS.sql
@@ -0,0 +1,9 @@
+SELECT   CONCAT(name, '(', SUBSTR(occupation, 1, 1), ')')
+FROM     occupations
+ORDER BY name;
+
+SELECT   CONCAT('There are a total of ', COUNT(occupation), ' ', lower(occupation), 's.')
+FROM     occupations
+GROUP BY occupation
+ORDER BY COUNT(occupation)
+        ,occupation;
