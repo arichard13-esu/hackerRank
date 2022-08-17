ff8915e6afde342f8b21bd00fb1a69ebafbb408b
Aaron <Aaron>
2022-08-17 11:33:21 -0400

    sWAP cASE solution

--- python/strings/sWAPcASE.py
+++ python/strings/sWAPcASE.py
@@ -1,7 +1,21 @@
 def swap_case(s):
-    return
+    # Solution 1
+    # return s.swapcase()
+    
+    # Solution 2
+    swap_string = ''
+    
+    for char in s:
+        if char.islower():
+            swap_string += char.upper()
+        elif char.isupper():
+            swap_string += char.lower()
+        else:
+            swap_string += char
+
+    return swap_string
 
 if __name__ == '__main__':
-    s = raw_input()
+    s = input()
     result = swap_case(s)
-    print result
+    print(result)
