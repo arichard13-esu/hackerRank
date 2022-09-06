439172a1acc57d56a9fc491576a1abab61b6b3c8
Aaron <Aaron>
2022-09-06 16:27:59 -0400

    added solution

--- python/basicDataTypes/nestedLists.py
+++ python/basicDataTypes/nestedLists.py
@@ -1,32 +1,35 @@
 if __name__ == '__main__':
-    # Create a nested list of students and grades
+    # Create a nested list of students and their grade
     grades = []
+    scorelist =[]
     for _ in range(int(input())):
         name = input()
         score = float(input())
         grades.append([name, score])
-    
+        # scorelist += [score]
+
+    # Sort list based on second element (grades)
     grades.sort(key=lambda x: x[1])
 
-    # Remove the lowest grade(s)
-    lowest_grade = min(grades, key=lambda x: x[1])
-    while lowest_grade in grades:
-        grades.remove(lowest_grade)
+    # Declare and remove the lowest grade
+    lowest_grade = min(grades, key=lambda x: x[1])[1]
+    while grades[0][1] == lowest_grade:
+        grades.pop(0)
 
-    i=0
+    # Declare a list of names of students with the second lowest grade
     names=[]
     lowest_grade = min(grades, key=lambda x: x[1])[1]
-    print(lowest_grade)
-    while True:
-        names.append(grades.pop[i][0])
-        i+=1
-        if grades[i][i] != lowest_grade:
-            break
-        
-    print(names)
-    # Create a list of names who have the second lowest grade(s)
-    
+    while len(grades) != 0 and grades[0][1] == lowest_grade:
+        names.append(grades.pop(0)[0])
+
+    # Sort and print the list of names
     names.sort()
     for name in names:
         print(name)
     
+    '''
+    b = sorted(list(set(scorelist)))[1] 
+    for a, c in sorted(grades):
+        if c == b:
+            print(a)
+    '''
--- sql/advancedJoin/placements.sql
+++ sql/advancedJoin/placements.sql
@@ -1,1 +1,7 @@
-
+SELECT name
+FROM students AS s
+JOIN friends AS f ON s.id = f.id
+JOIN packages AS sp ON f.id = sp.id
+JOIN packages AS fp ON f.friend_id = fp.id
+WHERE sp.salary < fp.salary
+ORDER BY fp.salary
